bdcebe73931e81788de906abc43954dee85aecd0
Pavel Novitskiy <quanbe921@gmail.com>
2022-04-23 16:46:40 +0300

    messagesPage update

--- src/components/Content/Dialogs/Messages/Messages.jsx
+++ src/components/Content/Dialogs/Messages/Messages.jsx
@@ -4,16 +4,16 @@ import s from './messages.module.css';
 const Message = (props) => {
     return (
         <div className={s.message}>
-        <div className={s.space}></div>
-        <div className={s.text}>
-            {props.text}
+            {/* <div className={s.space}></div> */}
+            <div className={s.text}>
+                {props.text}
+            </div>
+            {/* <div className={s.space}></div> */}
         </div>
-        <div className={s.space}></div>
-    </div>
     )
 };
 const Messages = (props) => {
-    let messages = props.dataMessages.map( message => <Message text={message.text}/>)
+    let messages = props.dataMessages.map(message => <Message text={message.text} />)
     return (
         <div className={s.body}>
             <div className={s.messages}>
@@ -21,7 +21,10 @@ const Messages = (props) => {
             </div>
 
             <div className={s.textInput}>
-                <input type='text' spellcheck='false' className={s.input}></input>
+                <input type='text' spellcheck='false' className={s.input} placeholder='Напишите сообщение...'></input>
+                <span className="material-symbols-outlined">
+                    send
+                </span>
             </div>
         </div>
     )
